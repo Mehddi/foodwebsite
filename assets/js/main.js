e6aae5c69477659c27a1b565779715b2cdaf0110
Mehddi <beloual.mehdi@yahoo.fr>
2024-10-31 20:41:31 +0100

    Menu swiper js

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*================ SHOW MENU ===========*/
+
+/*======= MENU SHOW =======*/
+/* Validate if constant exists */
+
+/*======= MENU HIDDEN =======*/
+/* Validate if constant exists */
+
+/*================ REMOVE MENU MOBILE ===========*/
+
+/*================ HOME SWIPER ===========*/
+var homeswiper = new Swiper('.home-swiper' , {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+/*================ DEALS TAB ===========*/
+
+/*================ SHOW SCROLL UP ===========*/
